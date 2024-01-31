1ec043ed810b41f23392c19f44fe4e6a1a7d9253
Shannon <s.russell3271@gmail.com>
2024-01-31 12:51:49 -0500

    Initial Commit GitHub Assignment

---
+++ icebreaker/icebreaker/icebreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  icebreakerApp.swift
+//  icebreaker
+//
+//  Created by Shannon Russell on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct icebreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
